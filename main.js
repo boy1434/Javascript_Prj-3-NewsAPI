07de36bfae557ba58215a24fa68b94c3fdf51a5e
boy1434@naver.com <boy1434@naver.com>
2024-02-23 22:03:08 +0900

    뉴스가져오기 render함수

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
 const API_KEY= `50b049f2fbce4d8f94a2df193555ac3d`
-let news = [];
+let newsList = [];
 // 뉴스를 가지고 오는 함수
  const getNews = async() => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
 
         // 데이터 요청
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log(news);
+    newsList = data.articles;
+    render();
+    console.log(newsList);
 }
+
+const render = () => {
+    let newsHTML = ``;
+    newsHTML = newsList.map(
+        (news) =>`
+     <div class="container row news">
+    <div class="col-lg-4 col-md-6">
+        <img src=${news.urlToImage} class="news-img-size">
+    </div>
+
+    <div class="col-lg-8 col-md-6">
+        <h2>${news.title}</h2>
+        <p>
+            ${news.description}
+        </p>
+        <div>
+            ${news.source.name} ${news.publishedAt}
+        </div>
+    </div>
+</div>`
+);
+
+    document.getElementById('news-board').innerHTML = newsHTML;
+}
+
 getNews();
