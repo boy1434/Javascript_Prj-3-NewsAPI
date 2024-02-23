647ab13d0f1825af8e0e806f48e8696d14afbc01
boy1434@naver.com <boy1434@naver.com>
2024-02-23 21:14:10 +0900

    async await 함수

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const API_KEY= `50b049f2fbce4d8f94a2df193555ac3d`
 
 // 뉴스를 가지고 오는 함수
-const getLatestNews = () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
-    console.log(url)
+ const getNews = async() => {
+    const url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+
+        // 데이터 요청
+    const response = await fetch(url);
+    console.log(response);
 }
-getLatestNews();
+getNews();
