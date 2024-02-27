54a2ac55987583d171d7b37577a43dead744249e
boy1434@naver.com <boy1434@naver.com>
2024-02-27 23:50:19 +0900

    카테고리별 보여주기

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const API_KEY= `50b049f2fbce4d8f94a2df193555ac3d`
 let newsList = [];
+const menus = document.querySelectorAll('.menus button');
+menus.forEach((menu) =>
+    menu.addEventListener("click", (e) => getNewsCategory(e) ) 
+    );
+
+    //1. 버튼에 클릭이벤트 주기
+const getNewsCategory = async(e) => {
+    const category = e.target.textContent.toLowerCase();
+    console.log(category);
+    const url = new URL(
+        `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`)
+
+    const response = await fetch(url);
+    const data = await response.json();
+    newsList = data.articles;
+    render();
+    console.log(data); 
+          
+}
+
 // 뉴스를 가지고 오는 함수
  const getNews = async() => {
     const url = new URL(
@@ -12,6 +32,8 @@ let newsList = [];
     render();
     console.log(newsList);
 }
+
+
 
 const render = () => {
     let newsHTML = ``;
@@ -38,3 +60,9 @@ const render = () => {
 }
 
 getNews();
+
+
+
+//2. 카테고리별 뉴스 가져오기
+//3. 가져온 카테고리별 뉴스 보여주기
+
